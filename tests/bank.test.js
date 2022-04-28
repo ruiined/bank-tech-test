11846af098fa8033ff190689d1d87af98e8538cb
ruiined <ruiined@gmail.com>
2022-04-28 19:47:27 +0100

    Deleted printer

---
+++ tests/bank.test.js
@@ -0,0 +1,87 @@
+const Bank = require("../src/bank");
+const Printer = require("../src/printer");
+const mockDate = require("jest-date-mock");
+
+const mockPrint = jest.fn();
+
+jest.mock("../src/printer", () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      print: mockPrint,
+    };
+  });
+});
+
+const date = new Date(2030, 0, 0, 0, 0, 0);
+
+describe("Bank", () => {
+  beforeEach(() => {
+    mockDate.advanceTo(date);
+  });
+
+  const bank = new Bank();
+  const printer = new Printer();
+
+  it("starts off with an empty balance and no transactions", () => {
+    expect(bank.transactions).toEqual([]);
+  });
+
+  it("deposits", () => {
+    bank.makeTransaction(500);
+    expect(bank.transactions[0].balance).toBe(500);
+  });
+
+  it("withdraws", () => {
+    bank.makeTransaction(-200);
+    expect(bank.transactions[1].balance).toBe(300);
+  });
+
+  it("stores the transactions with time and balance", () => {
+    expect(bank.transactions).toEqual([
+      {
+        date: new Date(date),
+        debit: 500,
+        balance: 500,
+      },
+      {
+        date: new Date(date),
+        credit: 200,
+        balance: 300,
+      },
+    ]);
+  });
+
+  it("prints the bank statement", () => {
+    bank.printStatement();
+    expect(printer.print).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe("Edge Cases", () => {
+  const bank = new Bank();
+  const amountError = "Invalid amount entered";
+
+  it("only takes positive or negative integers as an amount", () => {
+    expect(() => {
+      bank.makeTransaction(0);
+    }).toThrow(amountError);
+    expect(() => {
+      bank.makeTransaction("String");
+    }).toThrow(amountError);
+    expect(() => {
+      bank.makeTransaction({ Object });
+    }).toThrow(amountError);
+    expect(() => {
+      bank.makeTransaction();
+    }).toThrow(amountError);
+    expect(() => {
+      bank.makeTransaction(true);
+    }).toThrow(amountError);
+  });
+
+  it("throws an error when trying to withdraw more than what the balance is", () => {
+    expect(() => {
+      bank.makeTransaction(-30);
+    }).toThrow("Insufficient balance");
+  });
+});
